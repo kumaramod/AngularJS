2bfd6a42c0412d1ee3814ad55ce7e0504dbc3f66
amod_priya <amod_kumar@priyanet.com>
2017-02-14 16:54:04 +0530

    Added a solution file for angularjs with web api and mvc, for presentation and for small stuff demo

---
+++ AngularJSPresentation/Scripts/Directives.ctrl.js
@@ -0,0 +1,34 @@
+﻿
+var appDir = angular.module('myDirectives', []);
+appDir.controller('dirCtrl', function ($scope) {
+
+    $scope.init = function () {
+        
+        $scope.names = [
+{ name: 'Jani', country: 'Norway' },
+{ name: 'Hege', country: 'Sweden' },
+{ name: 'Kai', country: 'Denmark' }]
+    }
+
+    $scope.reverse = false;
+    $scope.predicate = '';
+    $scope.Test = "Amod";
+    $scope.SortObj = SortObj;
+
+    SortObj();
+    function SortObj(ord) {
+        $scope.predicate = ord;
+    }
+   
+    $scope.init();
+
+//    var self = this;
+//    self.init = function () {
+//        self.names = [
+//{ name: 'Jani', country: 'Norway' },
+//{ name: 'Hege', country: 'Sweden' },
+//{ name: 'Kai', country: 'Denmark' }]
+//    }
+//    self.init();
+
+});
---
+++ AngularJSPresentation/Scripts/ModuleController.ctrl.js
@@ -0,0 +1,35 @@
+﻿var app = angular.module('myapp', [])
+//.config(function (injectables) { // provider-injector
+// This is an example of config block.
+// You can have as many of these as you want.
+// You can only inject Providers (not instances)
+// into config blocks.
+//$provide.value('a', 123);
+//$provide.factory('a', function () { return 123; });
+//$compileProvider.directive('directiveName', ...);
+//$filterProvider.register('filterName', ...);
+
+//}).
+//run(function (injectables) { // instance-injector
+// This is an example of a run block.
+// You can have as many of these as you want.
+// You can only inject instances (not Providers)
+// into run blocks
+//});
+
+app.controller('ctrl', function ($scope) {
+
+    $scope.firstName = "First Name";
+    $scope.lastName = "Last Name";
+    $scope.myfullname = myFullName;
+
+    myFullName();
+    function myFullName() {
+        $scope.result = $scope.firstName + ' ' + $scope.lastName;
+    }
+
+
+    $scope.myName = function () { $scope.result = $scope.firstName + ' ' + $scope.lastName; };
+
+});
+
